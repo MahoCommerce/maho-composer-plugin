5af5b282705b09b1d7db62817363244bbe011c81
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-07-29 01:48:50 +0100

    fix

--- src/MahoComposerPlugin.php
+++ src/MahoComposerPlugin.php
@@ -99,7 +99,7 @@ class MahoComposerPlugin implements PluginInterface, EventSubscriberInterface
         }
 
         $dir = opendir($src);
-        @mkdir($dst);
+        mkdir($dst);
         while (false !== ($file = readdir($dir))) {
             if (($file != '.') && ($file != '..')) {
                 if (is_dir($src . '/' . $file)) {
